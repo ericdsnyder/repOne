22ba6dba776b08b4d5e622bc8b94726ab719ae59
ericdsnyder <ericdsnyder@live.com>
2016-11-10 15:11:40 -0600

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+* @author: Eric Snyder
+* @filename: main.cpp
+* @start: 11/10/2016
+* @brief: This is a basic "Hello World" program in C++
+*/
+
+#include <iostream>
+
+
+using namespace std;
+void main()
+{
+   cout << "Hello World!" << endl;   cout << "Welcome to Eric's C plus plus!" << endl; 
+}
